4f226bc650e8206ec3d10221d6dca680c1ba31ad
abdulbasit-dev <abdullbasit99salah@gmail.com>
2023-08-13 14:48:59 +0300

    login & logout api

--- app/Http/Controllers/API/V1/Auth/AuthController.php
+++ app/Http/Controllers/API/V1/Auth/AuthController.php
@@ -17,31 +17,22 @@ class AuthController extends Controller
     {
         //validation
         $validator = Validator::make($request->all(), [
-            'phone' => ['required', 'string'],
+            'email' => ['required', 'email'],
             'password' => ['required', 'string' , 'min:8'],
         ]);
 
         if ($validator->fails()) {
-            return $this->jsonResponse(false, __('api.invalid_inputs'), Response::HTTP_UNPROCESSABLE_ENTITY, null, $validator->errors()->all());
+            return $this->jsonResponse(false, __('Invalid inputs'), Response::HTTP_UNPROCESSABLE_ENTITY, null, $validator->errors()->all());
         }
 
         $credentials = $request->all();
         //check email
-        $user = User::where('phone', $credentials['phone'])->get()->first();
+        $user = User::where('email', $credentials['email'])->get()->first();
 
         //check password
         if (!$user || !Hash::check($credentials['password'], $user->password)) {
             return $this->jsonResponse(false, __('auth.failed'), Response::HTTP_UNAUTHORIZED);
         }
-
-        //check user if have uncompleted order, then show the table_no
-        $branch = $user->branch;
-        $userHasUncompletedOrder = Order::query()
-            ->where('branch_id', $branch->id)
-            ->where('user_id', $user->id)
-            ->where('status', OrderStatus::PENDING)
-            ->latest()
-            ->value('table_number');
 
         //create token
         $token = $user->createToken('myapitoken')->plainTextToken;
@@ -49,14 +40,15 @@ class AuthController extends Controller
         //get user role
         $user["user_role"] = $user->getRoleNames()[0] ?? null;
 
-        $user["table_number"] = $userHasUncompletedOrder;
+        // don't attach roles to user
+        $user->unsetRelation('roles');
 
-        return $this->jsonResponse(true, __('api.login_success'), Response::HTTP_OK, $user);
+        return $this->jsonResponse(true, __('Logged In Successfully!'), Response::HTTP_OK, $user);
     }
 
     public function logout()
     {
         auth()->user()->tokens()->delete();
-        return $this->jsonResponse(true, __('api.logout_success'), Response::HTTP_OK);
+        return $this->jsonResponse(true, __('Logged Out Successfully!'), Response::HTTP_OK);
     }
 }
--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,8 +5,41 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\Log;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    //set default pagination limitation
+    protected const ITEM_PER_PAGE = 20;
+
+    //return custom response
+    protected function jsonResponse($result = true, $message = "", $code = 200, $data = null, $error = null)
+    {
+        $response = [
+            'result' => $result,
+            'status' => $code,
+            'message' => $message,
+        ];
+
+        if ($data !== null || is_array($data)) {
+            $response['data'] = $data;
+        }
+
+        if ($error) {
+            $response['errors'] = $error;
+        }
+
+        return response()->json($response, $code);
+    }
+
+    public function error_log($exception)
+    {
+        Log::error([
+            $exception->getFile(),
+            $exception->getLine(),
+            $exception->getMessage()
+        ]);
+    }
 }
